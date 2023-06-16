548ec4a0739e2adf2aba9f68f7afda2bf3acfa7b
jhosno <jhosnoirlit@gmail.com>
2023-06-15 20:37:08 -0400

    UPDATE: state part

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import { useState } from "react";
-import './normalize.css'
+import "./normalize.css";
 import "./App.css";
 import Cards from "./components/Cards.jsx";
 import Navbar from "./components/Navbar";
-import characters from "./data.js";
+import axios from "axios";
+import Alert from "./components/Alert";
+import Footer from "./components/Footer";
+
+// import characters from "./data.js";
 function App() {
-  const [character, setCharacter] = useState([]);
-  const onSearch = () => {
-    setCharacter([...character]);
+  const [characters, setCharacters] = useState([]);
+  const IDChecker = (id) => {
+    //const result = characters.map((char) => char.id);
+    return characters.map((char) => char.id).includes(Number(id));
   };
+  function onSearch(id) {
+    if (IDChecker(id)) {
+      alert("ID repetido");
+    } else {
+      axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+        ({ data }) => {
+          if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+          } else {
+            window.alert("¡No hay personajes con este ID!");
+          }
+        }
+      );
+    }
+  }
+  const onClose = (id) => {
+    const result = characters.filter((char) => char.id !== id);
+    setCharacters(result);
+  };
+
   return (
-    <div className="App">
+    <div className="App" id="app">
       <Navbar onSearch={onSearch} />
-      <Cards characters={characters} />
+      <Cards characters={characters} onClose={onClose} />
+      <Footer />
     </div>
   );
 }
