19d0cfab84324427b16d8b764b829901cb65e675
jhosno <jhosnoirlit@gmail.com>
2023-06-20 11:18:12 -0400

    update: 404 error improvement

--- src/components/error404/Error.jsx
+++ src/components/error404/Error.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import "./error.css";
+import { NavLink } from "react-router-dom";
 
 const Error = () => {
-    return (
-        <div>
-            404
-        </div>
-    );
-}
+  return (
+    <div className="error___container">
+      <div className="error___text">
+        <h1>Are you lost?</h1>
+        <p>You can come back to home and rest. </p>
+        <NavLink to='/'>Take me back!</NavLink>
+      </div>
+    </div>
+  );
+};
 
 export default Error;
