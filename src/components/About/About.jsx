ad927ade401e5ccb070a9dfb77d29c32de626ea1
jhosno <jhosno.dev@gmail.com>
2023-07-07 18:13:48 -0400

    debug

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,23 +8,22 @@ const About = () => {
       <div className="about___card">
         <div className="about___card-content">
           <p>
-          Mi nombre es Jhosno (She/her).
-          
-            Me dedico a ayudar a diseñadores, organizaciones y negocios a
-            encontrar soluciones tecnológicas. Convierto la música dark wave en
-            código. Conmigo puedes conseguir resultados eficientes y sostenibles
-            a través de CMS, ecommerces y sistemas personalizados. Aparte de
-            renovar y mantener módulos o vistas. Adoro escuchar música, mi gata,
-            la ilustración y el diseño en muchas de sus vertientes. Estoy
-            enfocada en el trabajo frontend y trabajar en proyectos grandes y
-            enriquecedores de forma remota. Aquí en mi blog podrás encontrar
-            parte de mis experiencias y aprendizajes.
+            Mi nombre es Jhosno (She/her). Me dedico a ayudar a diseñadores,
+            organizaciones y negocios a encontrar soluciones tecnológicas.
+            Convierto la música dark wave en código. Conmigo puedes conseguir
+            resultados eficientes y sostenibles a través de CMS, ecommerces y
+            sistemas personalizados. Aparte de renovar y mantener módulos o
+            vistas. Adoro escuchar música, mi gata, la ilustración y el diseño
+            en muchas de sus vertientes. Estoy enfocada en el trabajo frontend y
+            trabajar en proyectos grandes y enriquecedores de forma remota. Aquí
+            en mi blog podrás encontrar parte de mis experiencias y
+            aprendizajes.
           </p>
           <div className="about___RRSS">
-            <a href="">Twitter</a>
-            <a href="">Github</a>
-            <a href="">LinkedIn</a>
-            <a href="">Site</a>
+            <a href="https://twitter.com/jhosno">Twitter</a>
+            <a href="https://github.com/jhosno">Github</a>
+            <a href="https://www.linkedin.com/in/jhosno/">LinkedIn</a>
+            <a href="https://jhosno.github.io">Site</a>
           </div>
         </div>
         <img
