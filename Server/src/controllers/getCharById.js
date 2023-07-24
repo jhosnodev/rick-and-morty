aec1b91e96c1f5172a09c8804c1aebbff07961e1
jhosno <jhosno.dev@gmail.com>
2023-07-23 21:22:24 -0400

    Create getCharById.js

---
+++ Server/src/controllers/getCharById.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+const URL = "http://localhost:3001/rickandmorty/character/";
+const getCharById = (res, id) => {
+  axios(`${URL}${id}`)
+    .then(({ data }) => {
+      const { name, gender, species, origin, image, status } = data;
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(id, name, gender, species, origin, image, status));
+    })
+    .catch((error) => {
+      res.writeHead(500, { "Content-type": "text/plain" });
+      res.end(error.message);
+    });
+};
+
+module.exports = getCharById;
